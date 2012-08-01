a5d13703341a07af564ac60f05985b7584e703e4
Lakshan Perera <lakshan@web2media.net>
2012-08-01 16:13:14 +0530

    add the list helper

---
+++ spec/helpers/list_helper.spec.js
@@ -0,0 +1,62 @@
+var list_helper_obj = require("../../lib/helpers/list_helper");
+var list_helper = list_helper_obj.directAccess();
+
+describe("format list", function(){
+
+	it("return empty strings for null values", function(){
+		expect(list_helper.format_list(null)).toEqual("");	
+	});
+
+	it("return the given string if it's not an array literal", function(){
+		expect(list_helper.format_list("hello")).toEqual("hello");	
+	});
+
+	it("return a formatted string for an array literal", function(){
+		expect(list_helper.format_list(["Peter", "John", "Andrew"])).toEqual("Peter, John &amp; Andrew");	
+	});
+
+	it("return a formatted string for an array literal as a string", function(){
+		expect(list_helper.format_list('Peter,John,Andrew')).toEqual("Peter, John &amp; Andrew");	
+	});
+
+});
+
+describe("first item of the list", function(){
+
+	it("return empty strings for null values", function(){
+		expect(list_helper.first(null)).toEqual("");	
+	});
+
+	it("return the given string if it's not an array literal", function(){
+		expect(list_helper.first("hello")).toEqual("hello");	
+	});
+
+	it("return a formatted string for an array literal", function(){
+		expect(list_helper.first(["Peter", "John", "Andrew"])).toEqual("Peter");	
+	});
+
+	it("return a formatted string for an array literal as a string", function(){
+		expect(list_helper.first('Peter,John,Andrew')).toEqual("Peter");	
+	});
+
+});
+
+describe("last item of the list", function(){
+
+	it("return empty strings for null values", function(){
+		expect(list_helper.last(null)).toEqual("");	
+	});
+
+	it("return the given string if it's not an array literal", function(){
+		expect(list_helper.last("hello")).toEqual("hello");	
+	});
+
+	it("return a formatted string for an array literal", function(){
+		expect(list_helper.last(["Peter", "John", "Andrew"])).toEqual("Andrew");	
+	});
+
+	it("return a formatted string for an array literal as a string", function(){
+		expect(list_helper.last('Peter,John,Andrew')).toEqual("Andrew");	
+	});
+
+});
