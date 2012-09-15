73e9f69733ce6c6f619758a045782017a650fd8b
Lakshan Perera <lakshan@web2media.net>
2012-09-16 05:24:09 +0530

    Allow setting markdown options in config

--- sample/contents/_service/content.markdown
+++ sample/contents/_service/content.markdown
@@ -2,4 +2,12 @@ This **is** a *markdown* formatted page.
 
 Isn't it cool?
 
+Here we try a code block:
+
+```ruby
+	def simple_method
+		puts "Punch is awesome"
+	end
+```
+
 ~~Nice~~
--- spec/parsers/markdown.spec.js
+++ spec/parsers/markdown.spec.js
@@ -1,9 +1,29 @@
 var markdown_parser = require("../../lib/parsers/markdown.js");
 var marked = require("marked");
 
+describe("setup", function() {
+
+	it("extend the marked options with the options provided in the config", function(){
+		var custom_options = { "gfm": true,  "pedantic": true, "sanitize": true }
+		var config = { "parser": { "markdown": custom_options } };
+
+		markdown_parser.setup(config);
+
+		expect(markdown_parser.markedOptions).toEqual(custom_options);
+	});
+
+	it("keep the default marked options when no options are provided in the config", function(){
+		var default_options = { "gfm": true,  "pedantic": false, "sanitize": false }
+
+		markdown_parser.setup({});
+
+		expect(markdown_parser.markedOptions).toEqual(default_options);
+	});
+});
+
 describe("parsing given content", function() {
 
-  it("calls the callback with the result", function() {
+  it("invoke the callback with the result", function() {
 		spyOn(marked, "setOptions");
 
 		spyOn(marked, "parse").andCallFake(function(input){
@@ -17,7 +37,7 @@ describe("parsing given content", function() {
 
   });
 
-  it("calls the callback with the result", function() {
+  it("invoke the callback with the error on an error", function() {
 		spyOn(marked, "setOptions");
 
 		spyOn(marked, "parse").andCallFake(function(input){
