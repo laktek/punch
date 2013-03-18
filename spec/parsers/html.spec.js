da486f9618b1af9ef3c466296a432b653ba64674
Sandy McArthur <Sandy@McArthur.org>
2013-03-18 01:21:50 -0400

    more space to tabs

--- spec/parsers/html.spec.js
+++ spec/parsers/html.spec.js
@@ -5,36 +5,36 @@ describe("setup", function() {
 	it("setup succeeds", function(){
 		html_parser.setup({});
 		html_parser.setup(undefined);
-        // anything to work
+		// anything to work
 	});
 
 });
 
 describe("parsing given content", function() {
 
-  it("invoke the callback with a string", function() {
+	it("invoke the callback with a string", function() {
 		var spyCallback = jasmine.createSpy();
 		html_parser.parse("testing 1 2 3", spyCallback);
 
 		expect(spyCallback).toHaveBeenCalledWith(undefined, "testing 1 2 3");
 
-  });
+	});
 
-  it("invoke the callback with empty content", function() {
+	it("invoke the callback with empty content", function() {
 		var spyCallback = jasmine.createSpy();
 		html_parser.parse("", spyCallback);
 
 		expect(spyCallback).toHaveBeenCalledWith(undefined, "");
 
-  });
+	});
 
-  it("invoke the callback with missing content causing an error", function() {
+	it("invoke the callback with missing content causing an error", function() {
 		var spyCallback = jasmine.createSpy();
 		html_parser.parse(undefined, spyCallback);
 
 		expect(spyCallback).toHaveBeenCalledWith({}, undefined);
 
-  });
+	});
 
 });
 
