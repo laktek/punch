795afa9a5bd55b0f9681925c4f27219900db74aa
Lakshan Perera <lakshan@web2media.net>
2013-09-14 15:09:25 +0800

    Log parsing errors of Less stylesheets. Also upgrade Less module to 1.4.1 or above.

--- spec/compilers/less.spec.js
+++ spec/compilers/less.spec.js
@@ -12,8 +12,8 @@ describe("calling compile", function() {
 		expect(Less.render).toHaveBeenCalledWith("test", { "filename": "sample/css/main.less", "paths": [ "sample/css" ] }, jasmine.any(Function));
 	});
 
-	it("calls the callback with the result", function(){
-		spyOn(Less, "render").andCallFake(function(input, options, callback){
+	it("calls the callback with the result", function() {
+		spyOn(Less, "render").andCallFake(function(input, options, callback) {
 			return callback(null, "rendered file");
 		});
 
@@ -21,6 +21,17 @@ describe("calling compile", function() {
 		LessCompiler.compile("test", "sample.less", spyCallback);
 
 		expect(spyCallback).toHaveBeenCalledWith(null, "rendered file");
+	});
+
+  it("calls the callback with the error message when an error occurrs", function() {
+		spyOn(Less, "render").andCallFake(function(input, options, callback){
+			return callback({ message: "Some error occurred."}, null);
+		});
+
+		var spyCallback = jasmine.createSpy();
+		LessCompiler.compile("test", "sample.less", spyCallback);
+
+		expect(spyCallback).toHaveBeenCalledWith("Less parsing error: Some error occurred.", null);
 	});
 
 });
